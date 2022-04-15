4ced213b7221e3e79f8d66434ad01ea2f7cd36cb
Sorin0404 <haewong27@gmail.com>
2022-04-15 17:57:01 +0900

    login page upload

---
+++ teamproject/todayhouse/todayhouse/templates/script/login.js
@@ -0,0 +1,9 @@
+function notCustomerOrderCheck() {
+  let x = document.getElementById("openOrderCheck");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
